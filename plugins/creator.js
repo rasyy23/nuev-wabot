2c1cbc7b84bd04b8e9a96fa3eed627f031ffabc2
muhammadhaikalpratama <muhammadhaikalpratama0897@gmail.com>
2024-10-22 12:08:20 +0700

    change fixes and watermark

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,9 +2,9 @@ function handler(m) {
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
-handler.help = ['owner', 'creator']
+handler.help = ['owner', 'creator','pemilik']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(owner|creator|pemilik)$/i
 
 export default handler
--- plugins/menu.js
+++ plugins/menu.js
@@ -153,7 +153,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
-    conn.sendHydrated(m.chat, text.trim(), author, pp, 'https://github.com/BochilGaming/games-wabot', 'Github', null, null, [
+    conn.sendHydrated(m.chat, text.trim(), author, pp, 'https://github.com/pxNuestra/nuev-wabot', 'Github', null, null, [
       ['Donate', '/donasi'],
       ['Speed', '/ping'],
       ['Owner', '/owner']
